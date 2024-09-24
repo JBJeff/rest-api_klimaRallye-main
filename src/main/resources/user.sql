d342f555516bae6103e703d8d6a160882e06b63f
JBjeff <boettcher-jeffrey@t-online.de>
2024-09-24 11:27:32 +0200

    User Passworts

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -2,5 +2,5 @@
 
 insert into Users (email,name,password) values ( 'josy@example.com', 'Josy','$2a$10$872ue7mTVhR9VYu69FptdempXQgDbF6tZS0kiuC4RhMCQB/Rwffzi');
 insert into Users (email,name,password) values ('gizem@example.com','Gizem','$2a$10$AEaDxC3hBw8EZ85988RlVOg1z/DQTKsDaT9PaQR64yPJg.Ro.oDUa');
-insert into Users (email,name,password) values ( 'philip@example.com','Philip','$2a$10$TLTD/lPPQzMDtN72Vs7EkOyMjH.qhWv8tWMLBL2AIuerNmPNoKsvS');
+insert into Users (email,name,password) values ( 'philip@example.com','Philip','$2a$10$jugAjraVMW8uRDLKWK6AheOKjlQIEsnfdVUVwH8sUnIc5NNMnoHua');
 insert into Users (email,name,password) values ('mohammed@example.com', 'Mohammed','$2a$10$TLTD/lPPQzMDtN72Vs7EkOyMjH.qhWv8tWMLBL2AIuerNmPNoKsvS');
